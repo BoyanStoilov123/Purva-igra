0e518a30d58e84475975f4664d068d502b4c51b5
etzorash-coder <et.yoan.h@eduiteh.eu>
2026-04-03 10:21:46 +0300

    After force push

--- obj/Release/net9.0/Purva-igra.AssemblyInfo.cs
+++ obj/Release/net9.0/Purva-igra.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Purva-igra")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a729d104dd9398ec6a2834926c9b519eff3d36b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Purva-igra")]
